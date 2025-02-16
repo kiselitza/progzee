aa5080300c2e3a6783031badfb52de4f10576a97
Aldin Kiselica <aldin.kiselica.94@gmail.com>
2025-02-16 08:09:31 +0100

    Update setup.py to include README to pypi

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="progzee",
     version="0.1.0",
     description="A Python library for simplifying IP proxy usage in HTTP requests, with proxy rotation.",
+    long_description = (this_directory / "README.md").read_text(),
     author="Aldin Kiselica",
     packages=find_packages(),
     install_requires=["requests", "click"],
